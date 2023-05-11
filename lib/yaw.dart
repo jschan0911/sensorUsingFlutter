932ce63ffbf24b0eb79eaadebcf98ebf42d1dd02
jeongseungchan <jschan0911@naver.com>
2023-05-11 15:50:35 +0900

    fix: 가속도계 센서 내용 제외

--- lib/yaw.dart
+++ lib/yaw.dart
@@ -26,55 +26,47 @@ class _ShowYawState extends State<ShowYaw> {
     super.initState();
     gyroscopeEvents.listen((GyroscopeEvent event) {
       setState(() {
-        // // Calculate deltaTime
-        // final double now = DateTime
-        //     .now()
-        //     .microsecondsSinceEpoch / 1000000;
-        // deltaTime = now - previousGyroscopeZ;
-        //
-        // // Update previousGyroscopeZ
-        // previousGyroscopeZ = now;
-        //
-        // // Update gyroscopeZ
-        // gyroscopeZ += event.z * deltaTime;
+        // gYaw += (180 / pi) * event.z * 0.01;
+        yaw += (180 / pi) * event.z * 0.01;
 
-        // Update yaw using gyroscopeZ
-        // yaw += (180 / pi) * gyroscopeZ;
-        //
-        // gyroscopeZ = 0;
+        if (yaw > 90) {
+          detection = "Left";
+          yaw = 0;
+        }
 
-        gYaw += (180 / pi) * event.z * 0.01;
-
-        // if (yaw > 90) {
-        //   detection = "Left";
-        //   yaw = 0;
-        // }
-        //
-        // if (yaw < -90) {
-        //   detection = "Right";
-        //   yaw = 0;
-        // }
+        if (yaw < -90) {
+          detection = "Right";
+          yaw = 0;
+        }
       });
     });
 
-    accelerometerEvents.listen((AccelerometerEvent event) {
-      setState(() {
-        double gx = event.x;
-        double gy = event.y;
-        double gz = event.z;
-
-        double norm = sqrt(gx * gx + gy * gy + gz * gz);
-        gx /= norm;
-        gy /= norm;
-        gz /= norm;
-
-        double pitch = asin(-gx);
-        double roll = atan2(gy, gz);
-
-        aYaw = -atan2(event.y, event.z);
-        yaw = gYaw * 0.98 + aYaw * 0.02;
-      });
-    });
+    // accelerometerEvents.listen((AccelerometerEvent event) {
+    //   setState(() {
+    //     double gx = event.x;
+    //     double gy = event.y;
+    //     double gz = event.z;
+    //
+    //     double norm = sqrt(gx * gx + gy * gy + gz * gz);
+    //     gx /= norm;
+    //     gy /= norm;
+    //     gz /= norm;
+    //
+    //     aYaw += (180 / pi) * -atan2(gx, gz);
+    //     yaw = gYaw * 0.98 + aYaw * 0.02;
+    //     // yaw = aYaw;
+    //
+    //     if (yaw > 90) {
+    //       detection = "Left";
+    //       yaw = 0;
+    //     }
+    //
+    //     if (yaw < -90) {
+    //       detection = "Right";
+    //       yaw = 0;
+    //     }
+    //   });
+    // });
   }
 
   @override
@@ -94,9 +86,9 @@ class _ShowYawState extends State<ShowYaw> {
               'Detect: $detection',
               style: TextStyle(fontSize: 24.0),
             ),
-            TextButton(onPressed: (){
-              yaw = 0;
-            }, child: Text("Reset"))
+            // TextButton(onPressed: (){
+            //   yaw = 0;
+            // }, child: Text("Reset"))
           ],
         ),
       )
