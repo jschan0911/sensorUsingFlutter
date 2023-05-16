a2eea04fe196d04cff3b7d010ef76afdc581e70e
jeongseungchan <jschan0911@naver.com>
2023-05-16 11:22:59 +0900

    style: QR코드 인식 페이지 UI 수정

--- lib/qrPage.dart
+++ lib/qrPage.dart
@@ -32,85 +32,17 @@ class _QRPageState extends State<QRPage> {
       body: Column(
         children: [
           Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(child: FittedBox(
-            fit: BoxFit.contain,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                if(result != null)
-                  // Text('Data: ${result!.code}')
-                  ElevatedButton(onPressed: () {
-                    _navigationToNextPage(context, result!.code!);
-                  }, child: Text('${result!.code}'))
-                else
-                  const Text('Scan a code'),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.center,
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //   children: <Widget>[
-                //     Container(
-                //       margin: const EdgeInsets.all(8),
-                //       child: ElevatedButton(
-                //           onPressed: () async {
-                //             await controller?.toggleFlash();
-                //             setState(() {});
-                //           },
-                //           child: FutureBuilder(
-                //             future: controller?.getFlashStatus(),
-                //             builder: (context, snapshot) {
-                //               return Text('Flash: ${snapshot.data}');
-                //             },
-                //           )),
-                //     ),
-                //     // Container(
-                //     //   margin: const EdgeInsets.all(8),
-                //     //   child: ElevatedButton(
-                //     //       onPressed: () async {
-                //     //         await controller?.flipCamera();
-                //     //         setState(() {});
-                //     //       },
-                //     //       child: FutureBuilder(
-                //     //         future: controller?.getCameraInfo(),
-                //     //         builder: (context, snapshot) {
-                //     //           if (snapshot.data != null) {
-                //     //             return Text(
-                //     //                 'Camera facing ${describeEnum(snapshot.data!)}');
-                //     //           } else {
-                //     //             return const Text('loading');
-                //     //           }
-                //     //         },
-                //     //       )),
-                //     // )
-                //   ],
-                // ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.center,
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //   children: <Widget>[
-                //     Container(
-                //       margin: const EdgeInsets.all(8),
-                //       child: ElevatedButton(
-                //         onPressed: () async {
-                //           await controller?.pauseCamera();
-                //         },
-                //         child: const Text('pause',
-                //             style: TextStyle(fontSize: 20)),
-                //       ),
-                //     ),
-                //     Container(
-                //       margin: const EdgeInsets.all(8),
-                //       child: ElevatedButton(
-                //         onPressed: () async {
-                //           await controller?.resumeCamera();
-                //         },
-                //         child: const Text('resume',
-                //             style: TextStyle(fontSize: 20)),
-                //       ),
-                //     )
-                //   ],
-                // ),
-              ],
-            ),
+          Expanded(child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              if(result != null)
+                // Text('Data: ${result!.code}')
+                ElevatedButton(onPressed: () {
+                  _navigationToNextPage(context, result!.code!);
+                }, child: Text('${result!.code}'))
+              else
+                const Text('Scan a code'),
+            ],
           ))
         ],
       ),
