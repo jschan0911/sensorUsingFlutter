442c3aff257003cc681dcf5dbb519abeb71c95ae
jeongseungchan <jschan0911@naver.com>
2023-05-18 19:39:15 +0900

    style: DB 테스트 페이지 타이틀 수정

--- lib/postgres.dart
+++ lib/postgres.dart
@@ -22,13 +22,13 @@ class _PostgresPageState extends State<PostgresPage> {
     await connection.open();
 
     List<List<dynamic>> results = await connection.query(query);
-    String _text = results[0][1];
+    // String _text = results[0][1];
     print(results);
-    print(_text);
+    // print(_text);
 
     //이 부분 블로깅하자 setState를 안 해서 반영이 안 되는 문제 많이 겪었어 ㅎㅎ
     setState(() {
-      text = _text;
+      // text = _text;
     });
   }
 
@@ -36,13 +36,13 @@ class _PostgresPageState extends State<PostgresPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter PostgreSQL2 Demo'),
+        title: Text('Postgres 패키지 실험'),
       ),
       body: Center(
         child: ElevatedButton(
           child: Text(text),
           onPressed: () {
-            String query = 'SELECT * FROM account_test';
+            String query = "SELECT * FROM pgr_dijkstra('SELECT id, source, target, cost, reverse_cost FROM edge_table_1f_5',2, 7);";
             ExecuteQuery(query);
           },
         ),
