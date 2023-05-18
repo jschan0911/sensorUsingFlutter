8f279ace6d4f7db8780572d3740f1e438f1dd101
jeongseungchan <jschan0911@naver.com>
2023-05-18 20:16:53 +0900

    style: 가속도 센서로부터 0.05초 단위로 값을 불러오되, 0.5초 단위로 한계값을 넘었는지 검증함으로 걸음 수를 계산하도록 수정

--- lib/getPath.dart
+++ lib/getPath.dart
@@ -9,38 +9,51 @@ class GetPath extends StatefulWidget {
 }
 
 class _GetPathState extends State<GetPath> {
+  bool _isOver = false;
+
+  int _step = 0;
+
   double _accelerometerX = 0.0;
   double _accelerometerY = 0.0;
   double _accelerometerZ = 0.0;
-
   double _svm = 0.0;
+  double _threshold = 7.0;
 
-  String _detect = "";
-
-  int _step = 0;
+  // String _detect = "";
 
   @override
   void initState() {
     DateTime beforeTime = DateTime.now();
+    DateTime beforeTimeForSensor = beforeTime;
     // TODO: implement initState
     super.initState();
     accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
         DateTime nowTime = DateTime.now();
-        int deltaTime = nowTime.difference(beforeTime).inMilliseconds;
+        DateTime nowTimeForSensor = nowTime;
 
-        if (deltaTime >= 100) {
+        int deltaTime = nowTime.difference(beforeTime).inMilliseconds;
+        int deltaTimeForSensor = nowTimeForSensor.difference(beforeTimeForSensor).inMilliseconds;
+
+        if (deltaTimeForSensor >= 50) {
           _accelerometerX = event.x;
           _accelerometerY = event.y;
           _accelerometerZ = event.z;
-
           _svm = _accelerometerX.abs() + _accelerometerY.abs() + _accelerometerZ.abs() - 10.0;
 
-          if (_svm > 7.0) {
-            _detect += "\n$_svm";
-            _step += 1;
+          beforeTimeForSensor = nowTimeForSensor;
+
+          if (_svm > _threshold) {
+            _isOver = true;
           }
-          beforeTime = nowTime;
+
+          if (deltaTime >= 500) {
+            if (_isOver == true) {
+              _isOver = false;
+              _step += 1;
+              beforeTime = nowTime;
+            }
+          }
         }
       });
     });
@@ -50,18 +63,16 @@ class _GetPathState extends State<GetPath> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Get Path"),),
-      body: Column(
-        children: [
-          Text("$_svm"),
-          Text("$_step"),
-          ListView(
-            children: [
-              Center(
-                child: Text(_detect),
-              )
-            ]
-          ),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            Text("$_svm"),
+            Text("$_step", style: TextStyle(fontSize: 30),),
+            ElevatedButton(onPressed: () {
+              _step = 0;
+            }, child: Text("Reset")),
+          ],
+        ),
       ),
     );
   }
